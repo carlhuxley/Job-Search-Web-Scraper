63edcd1dc964711f2f4080269f1d5fa95407b653
Carl Huxley <carlhuxley@gmail.com>
2019-10-22 06:27:49 +0100

    Change init.py conf settings

--- job_func/__init__.py
+++ job_func/__init__.py
@@ -9,11 +9,10 @@ from datetime import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from job_func.job_search import job_search
+from job_func.config import Config
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
-
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 
 from job_func import routes
